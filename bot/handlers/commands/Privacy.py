52ec4ea52422ee69dc19c15bd2fbfac47058e7de
Tirik8 <yaroslavsemenoviop@gmail.com>
2024-07-22 15:12:38 +0300

    privacy

--- bot/handlers/commands/Privacy.py
+++ bot/handlers/commands/Privacy.py
@@ -8,6 +8,8 @@ router = Router()
 @router.message(Command("privacy"))
 async def handler_command_start(message: Message):
     await message.answer(
-        f"Политика конфиденциальности",
+        f"Политика конфиденциальности\n\n",
+        f"Сервис хранит информацию о пользователях, подключивших его, на протяжении всего сипользования и 30 дней после отключения от сервиса",
+        
         reply_markup=ReplyKeyboardRemove(),
     )
